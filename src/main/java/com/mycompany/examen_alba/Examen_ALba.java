134faae711a4dacbba51c6060bd87c93e4d254e6
Alba <albag95591@gmail.com>
2024-12-11 14:05:59 +0100

    Examen Alba

---
+++ src/main/java/com/mycompany/examen_alba/Examen_ALba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.examen_alba;
+
+/**
+ *
+ * @author Alba
+ */
+public class Examen_ALba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
